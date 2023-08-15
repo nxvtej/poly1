700a1b70aa77236955693ab6747bd37c643bac92
Navdeep <108007267+nxvtej@users.noreply.github.com>
2023-08-15 17:44:06 +0530

    Add files via upload

---
+++ metadata/contractAddress.js
@@ -0,0 +1,3 @@
+
+    export const nftAddress = "0x93b997E3669CAADf955e68c6c97E49E13c80b444"
+  
---
+++ metadata/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+    {
+        name: '0',
+        description: 'Portrait of young woman. Number 0',
+        image: 'QmT8LZaEB2KEHH1fv7VHhpcYdxK4KbHqSgAd8knV1FmYtB'
+    },
+    {
+        name: '1',
+        description: 'Portrait of young woman. Number 1',
+        image: 'QmUc3qoeB9NRX49h7hXv9iqCgGjtxTPEgkVvw1SXkY8mJW'
+    },//change
+    {
+        name: '2',
+        description: 'Portrait of young woman. Number 2',
+        image: 'QmNjFnt872KagWybbkbnUyx9adyzqme961idJFM8wwQFDP'
+    },
+    {
+        name: '3',
+        description: 'Portrait of young woman. Number 3',
+        image: 'QmPDEE7jqoGmpHdJGSqvSoz9FDDVCMUE7iJcS51CfUqruQ'
+    },
+    {
+        name: '4',
+        description: 'Portrait of young woman. number 4',
+        image: 'QmaXunfCYGKhxxZV2GotYwHjPT9QkmAyPzyn6zPfYYwtat'
+    },    
+];
+
+module.exports = nfts;
